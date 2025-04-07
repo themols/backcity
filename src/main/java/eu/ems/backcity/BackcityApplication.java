72c31b6df8198f6f14d281215ac0ac0eb586a4b5
Eduardo Molina <eduardo@azulsystems.com.br>
2025-04-07 19:29:44 -0300

    init

---
+++ src/main/java/eu/ems/backcity/BackcityApplication.java
@@ -0,0 +1,13 @@
+package eu.ems.backcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackcityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackcityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/ems/backcity/BackcityApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.ems.backcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackcityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
